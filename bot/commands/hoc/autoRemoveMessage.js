a1e0b81bf26582019d633f066067ec42012e1a8e
Aron Bergman <aron@bergman.re>
2024-04-01 20:46:48 +0200

    feat(core): counter for auto remove messages

--- bot/commands/hoc/autoRemoveMessage.js
+++ bot/commands/hoc/autoRemoveMessage.js
@@ -1,7 +1,23 @@
 export const autoRemoveMessage = async (content, bot, chatId, options, duration = 5000) => {
-  const message = await bot.sendMessage(chatId, content, options)
+  const message = await bot.sendMessage(chatId, `🚀 \n\n${content}`, options)
+  const durationTemplate = '・'
 
-  setTimeout((bot, chatId, message) => {
+  for (let i = 0; duration / 1000 >= i; i++) {
+    const timeout = setTimeout((bot, chatId, message, durationTemplate, duration) => {
+      bot.editMessageText(
+        `🚀 ${durationTemplate.repeat(duration / 1000 - i)}\n\n${content}`,
+        {
+          chat_id: chatId,
+          message_id: message.message_id,
+          parse_mode: 'HTML'
+        }
+      )
+      clearTimeout(timeout)
+    }, i * 1000, bot, chatId, message, durationTemplate, duration)
+  }
+
+  const remove = setTimeout((bot, chatId, message) => {
     bot.deleteMessage(chatId, message.message_id)
+    clearTimeout(remove)
   }, duration, bot, chatId, message)
 }
