190e800ff1c7f54e6f5b025b3bbd79b382cc1505
Aron Bergman <aron@bergman.re>
2024-04-26 00:41:27 +0200

    feat(converter): add preloader and types

--- bot/commands/keyboard/converter.js
+++ bot/commands/keyboard/converter.js
@@ -3,6 +3,14 @@ import { GetSupportedConversionTypesRequest, InfoApi } from 'groupdocs-conversio
 import { Converter } from '../../utils/converter.js'
 
 export const keyboardConverter = async (bot, msg) => {
+  let accountMessage = await bot.sendMessage(
+    msg.chat.id,
+    '💱',
+    {
+      parse_mode: 'HTML'
+    }
+  ).catch(err => console.log(err))
+
   // прислать сообщение с инструкцией
   // принять файл который пользователь хочет конвертитьвать
   // записать информацию о том, что файл загружен, какое у него имя, формат, в какой формат, статус
@@ -13,10 +21,18 @@ export const keyboardConverter = async (bot, msg) => {
   const converter = new Converter()
   const formats = await converter.getSupportedConversionTypes()
 
-  await bot.sendMessage(msg.chat.id, `
-  В этом режиме ты можешь конвертировать файлы. Просто пришли файл в чат и выбери формат для конвертации.\n\nПоддерживаемые форматы: ${formats.map((i) => i.sourceFormat).join(', ')}`)
-    .catch((error) => {
-      console.log('Error: ' + error.message)
-    })
+  console.log('formats', formats)
 
+  const timeout = setTimeout((chatId, message_id) => {
+    bot.editMessageText(
+      `В этом режиме ты можешь конвертировать файлы. Просто пришли файл в чат и выбери формат для конвертации.\n\nПоддерживаемые форматы: ${formats.map((i) => i.sourceFormat).join(', ')}`
+      , {
+        chat_id: chatId,
+        message_id: message_id,
+        parse_mode: 'HTML'
+      }).catch(() => {
+      return true
+    })
+    clearTimeout(timeout)
+  }, 0, msg.chat.id, accountMessage?.message_id)
 }
